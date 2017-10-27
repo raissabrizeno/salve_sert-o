042252318d9a2423ea44d828633b55658318df24
Raissa Brizeno <raissa.brizeno@gmail.com>
2017-10-27 00:34:04 -0300

    alteração da página inicial

---
+++ index.php
@@ -0,0 +1,255 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Salve Sertão</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="css/mystyle.css" rel="stylesheet">
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+
+    <!-- Plugin CSS -->
+    <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
+
+    <!-- Theme CSS -->
+    <link href="css/creative.min.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body id="page-top">
+
+    <nav id="mainNav" class="navbar navbar-default navbar-fixed-top">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand page-scroll" href="#page-top">Salve Sertão</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a class="page-scroll" href="#about">Sobre</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#services">Institucional</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#portfolio">Galeria</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#contact">Atividades</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#contact">Agenda</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#contact">Fale Conosco</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+
+    <header>
+        <div class="header-content">
+            <div class="header-content-inner">
+                <h1 id="homeHeading" style="text-shadow: 0px 0px 20px black;">Seja bem vindo</h1>
+                <hr>
+                <a href="#about" class="btn btn-primary btn-xl page-scroll">Saiba mais</a>
+            </div>
+        </div>
+    </header>
+
+    <section class="bg-primary" id="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-7 col-md-6">
+                    <h2 class="section-heading">Nossa História</h2>
+                    <hr class="light">
+
+                    <p class="text-faded" text-align="left">Em meados de 2012, a Região Nordeste foi atingida por uma situação de emergência decorrente da seca, onde a falta de chuva secou rios; prejudicou as lavouras, pois na terra esturricada não dava para plantar; deixando os animais famintos, sem ter o que comer; fez disparar o preço de alimentos; em resumo, um cenário de total desolação.
+
+Quando se tornou professor, o Fundador Guido Cavalcanti sentiu que, a melhor maneira de ensinar era colocar o aluno diante do caso prático. Diante disso, o melhor caso prático possível seria o problema social, fornecendo aprendizado e uma visão ampla, desperta e profundamente maior, através do conhecimento ministrado dentro de sala de aula e do envolvimento do aluno nas ações em si, fazendo um ciclo perfeito de sustentabilidade entre: aprendizado, solidariedade e auxílio.
+
+Com objetivo de minimizar – no que for possível – o sofrimento dos sertanejos atingidos pela longa estiagem, nasceu no meio universitário a ONG Salve Sertão, entidade de iniciativa privada, sem fins lucrativos, sem filiação partidária ou instintuições religiosas, tendo como Fundador o Professor Guido Cavalcanti e, sendo administrada por voluntários.</p>
+                </div>
+                <div class="col-lg-5 col-md-6 video">
+                    <iframe width="400" height="350" src="https://www.youtube.com/embed/wOjw57mk5O4" frameborder="0" gesture="media" allowfullscreen align="right"></iframe>
+                </div>
+            </div>
+            <div class="col-lg-12 col-md-12">
+              <a href="#services" class="page-scroll btn btn-default btn-xl sr-button"><i class="fa fa-chevron-down"></i></a>
+            </div>
+        </div>
+    </section>
+
+    <section id="services">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2 class="section-heading">Princípios</h2>
+                    <hr class="primary">
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-md-6 text-center">
+                    <div class="service-box">
+                        <i class="fa fa-5x fa-map-signs text-primary sr-icons"></i>
+                        <h3>Missão</h3>
+                        <p class="text-muted">Dedicar-se, com sinceridade, parte de sua energia e criatividade em favor do mundo. E cada vez mais, ampliar sua capacidade de amar.</p>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 text-center">
+                    <div class="service-box">
+                        <i class="fa fa-5x fa-heart-o text-primary sr-icons"></i>
+                        <h3>Valores</h3>
+                        <p class="text-muted">Amor, humildade, compaixão, solidariedade, humanismo, desenvolvimento do ser, conhecimento, respeito, carinho, transformação, resiliência, transmutação, ética e transparência.</p>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 text-center">
+                    <div class="service-box">
+                        <i class="fa fa-5x fa-rocket text-primary sr-icons"></i>
+                        <h3>Visão</h3>
+                        <p class="text-muted">Somos um grupo humanista na tentativa sincera de amar ao próximo, buscando crescer na solidariedade, na caridade e no profissionalismo.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="no-padding" id="portfolio">
+        <div class="container-fluid">
+            <div class="row no-gutter popup-gallery">
+                <div class="col-lg-3 col-sm-6">
+                    <a href="img/portfolio/fullsize/1.jpg" class="portfolio-box" style="height: 229px; overflow: hidden;">
+                        <img src="img/portfolio/thumbnails/1.jpg" class="img-responsive" alt="" style="width: auto; height: 100%; max-width: unset;">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded">
+                                    Categoria
+                                </div>
+                                <div class="project-name">
+                                    Gastronomia
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                    <a href="img/portfolio/fullsize/2.jpg" class="portfolio-box" style="height: 229px; overflow: hidden;">
+                        <img src="img/portfolio/thumbnails/2.jpg" class="img-responsive" alt="" style="width: auto; height: 100%; max-width: unset;">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded">
+                                    Categoria
+                                </div>
+                                <div class="project-name">
+                                    Hotelaria
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                    <a href="img/portfolio/fullsize/3.jpg" class="portfolio-box" style="height: 229px; overflow: hidden;">
+                        <img src="img/portfolio/thumbnails/3.jpg" class="img-responsive" alt="" style="width: auto; height: 100%; max-width: unset;">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded">
+                                    Categoria
+                                </div>
+                                <div class="project-name">
+                                    Igrejas
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                    <a href="img/portfolio/fullsize/4.jpg" class="portfolio-box" style="height: 229px; overflow: hidden;">
+                        <img src="img/portfolio/thumbnails/4.jpg" class="img-responsive" alt="" style="width: auto; height: 100%; max-width: unset;">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded">
+                                    Categoria
+                                </div>
+                                <div class="project-name">
+                                    Monumentos
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <aside class="bg-dark">
+        <div class="container text-center">
+            <div class="call-to-action">
+                <h2>Download gratuito na Google Play store!</h2>
+                <a href="javaScript:alert('Em breve!');" class="btn btn-default btn-xl sr-button">Baixe agora!</a>
+            </div>
+        </div>
+    </aside>
+
+    <section id="contact">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 text-center">
+                    <h2 class="section-heading">Entre em contato</h2>
+                    <hr class="primary">
+                    <p>Preparado para aproveitar tudo o que Olinda tem a oferecer? Ótimo! Entre em contato conosco!</p>
+                </div>
+                <div class="col-lg-12 text-center">
+                    <i class="fa fa-envelope-o fa-3x sr-contact"></i>
+                    <p><a href="mailto:your-email@your-domain.com">contatoguiaolinda@gmail.com</a></p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- jQuery -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+    <script src="vendor/scrollreveal/scrollreveal.min.js"></script>
+    <script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+
+    <!-- Theme JavaScript -->
+    <script src="js/creative.min.js"></script>
+
+</body>
+
+</html>
